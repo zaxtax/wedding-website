fca7116e5db4e2724e72dab8ccd1fbfd9afc2981
Rob Zinkov <rob@zinkov.com>
2026-05-07 00:41:05 +0200

    Fix clock

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2026-09-06 17:00", "USA/Eastern Standard Time");
+  let targetDate = moment.tz("2026-09-06 15:00", "America/New_York");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
